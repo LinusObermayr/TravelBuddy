6ac50c60ef66c64ca5d0df3e9bd9342433d03176
LinusObermayr <91469366+LinusObermayr@users.noreply.github.com>
2023-04-18 09:50:50 +0200

    Initial Commit

---
+++ TravelBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravelBuddy
+//
+//  Created by Linus Obermayr on 2023-04-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TravelBuddy/TravelBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelBuddyApp.swift
+//  TravelBuddy
+//
+//  Created by Linus Obermayr on 2023-04-18.
+//
+
+import SwiftUI
+
+@main
+struct TravelBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
